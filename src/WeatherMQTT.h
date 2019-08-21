097b4334eca860f98b2e6781dc773791b0d4195e
Stefan Andres <develop@myandres.de>
2019-08-21 02:52:37 +0200

    GPL Comments

--- src/WeatherMQTT.h
+++ src/WeatherMQTT.h
@@ -1,10 +1,20 @@
 /*
- * WeatherMQTT.h
- *
- *  Created on: 06.02.2019
- *      Author: stefan
- */
+WeatherMQTT.h - Include file for class "WeatherMQTT".
+Copyright (C) 2019  by Stefan Andres
 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 #ifndef WEATHERMQTT_H_
 #define WEATHERMQTT_H_
 
